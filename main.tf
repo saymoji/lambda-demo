a285c7175e572c2f7c09152b1ee0a83e7ba2ad51
nalbam <me@nalbam.com>
2018-06-12 12:02:54 +0900

    tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+# Terraform Main
+
+module "domain" {
+  source = "git::https://github.com/nalbam/terraform-aws-route53.git"
+  domain = "${var.domain}"
+}
+
+module "demo-api" {
+  source = "git::https://github.com/nalbam/terraform-aws-lambda-api.git"
+  region = "${var.region}"
+
+  name         = "${var.name}"
+  stage        = "${var.stage}"
+  description  = "route53 > api gateway > lambda"
+  runtime      = "nodejs8.10"
+  handler      = "index.handler"
+  memory_size  = 512
+  timeout      = 5
+  s3_bucket    = "${var.s3_bucket}"
+  s3_source    = "target/lambda.zip"
+  s3_key       = "lambda/${var.name}/${var.name}-${var.version}.zip"
+  http_methods = ["ANY"]
+
+  zone_id         = "${module.domain.zone_id}"
+  certificate_arn = "${module.domain.certificate_arn}"
+  domain_name     = "${var.name}.${var.domain}"
+
+  env_vars = {
+    PROFILE = "${var.stage}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable region {
+  default = "ap-northeast-2"
+}
+
+variable name {
+  default = "demo-api"
+}
+
+variable stage {
+  default = "dev"
+}
+
+variable domain {
+  default = "saymoji.com"
+}
+
+variable s3_bucket {
+  default = "repo.saymoji.com"
+}
+
+variable version {
+  default = "1.0.0"
+}
